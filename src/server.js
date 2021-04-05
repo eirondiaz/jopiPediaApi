c9c999fbc198a8deae6e382f70282cc608ce2e6d
EironDiaz <eirondiaz@gmail.com>
2021-04-05 12:32:21 -0400

    api reparada en heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const morgan = require('morgan')
 const swaggerUI = require('swagger-ui-express')
 const swaggerDocs = require('./swagger-doc')
 require('./database')
@@ -13,7 +12,6 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 //middlewares
 app.use(cors())
-app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
